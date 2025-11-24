bc9a07ebe486b2b4a0f9022eab9297e678dcbdc5
= <=>
2025-11-24 23:24:04 +0530

    added song

--- src/data/songs.js
+++ src/data/songs.js
@@ -16,6 +16,7 @@ export const songs = [
   'Poyidam',
   'Ponnallle poovalle ',
   'Anaswara Sneha Swaroopanayi',
+  'Daivam pirakkunnu'
 
 ];
 
@@ -324,6 +325,25 @@ Mannavanayitha Vannu Pirannu
 
 Imbamayi Paaduka.......... 
 Anaswara Sneha............. 
-`
+`,
+`Daivam pirakkunnu 
+Manushyanaay bethlahemil 
+Manju peyyunna malarmadakkil 
+Halleluya Halleluya.. 
+Mannilum vinnilum 
+mandhahaasam peyyum 
+Madhura manohara gaanam (2)
 
+Halleluya Halleluya.. 
+Halleluya Halleluuuuuya.. (Daivam pirakkunnu)
+
+Paathiraavil manjeteeranaay 
+Paarinte naadhan pirakkukayaay (2) 
+Paadiyaarkoo veenameetoo 
+Daivathin daasare onnu cheroo (2) (Daivam pirakkunnu)
+
+Pakalonu munpe pithaavinte hrithile.. 
+Thriyeka soonuvaamudhaya sooryan (2) 
+Praabhava poornnanaayuyarunnitha... 
+Prathaapamodinneshu naathan.. (2) (Daivam pirakkunnu)`
 ];
